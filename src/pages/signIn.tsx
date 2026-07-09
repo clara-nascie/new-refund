1f09a7cb5cf274df02e206f38cb848cc6f64ef2c
Clara Nascimento <clara.nasc13@gmail.com>
2026-07-09 18:04:52 -0300

    feat: implement sign-in page with loading state and update TypeScript path configuration

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,14 +1,33 @@
+import { useState, type SubmitEvent } from "react";
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function onSubmit(e: SubmitEvent) {
+    e.preventDefault();
+
+    //carregar o botão deixando ele desabilitado temporariamente 
+    setIsLoading(true);
+
+    //o botão volta ao normal depois de 2s
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    console.log(email, password);
+  }
   return (
-    <form className="w-full flex flex-col gap-4">
+    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input
         required
         legend="email"
         type="email"
         placeholder="Digite seu e-mail"
+        onChange={(e) => setEmail(e.target.value)}
       />
 
       <Input
@@ -16,8 +35,11 @@ export function SignIn() {
         legend="senha"
         type="password"
         placeholder="Digite sua senha"
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <Button>Entrar</Button>
+      <Button type="submit" isLoading={isLoading}>
+        Entrar
+      </Button>
     </form>
   );
 }
