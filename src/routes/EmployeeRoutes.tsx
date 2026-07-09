2479bee782574900efd19aecfe0791adc0a253c0
Clara Nascimento <clara.nasc13@gmail.com>
2026-07-09 19:31:18 -0300

    feat: implement AppLayout component and define employee routing structure

--- src/routes/EmployeeRoutes.tsx
+++ src/routes/EmployeeRoutes.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router";
+
 import { Refund } from "../pages/Refund";
 import { NotFound } from "../pages/notFound";
+import { AppLayout } from "@/components/AppLayout";
 
 
 export function EmployeeRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<Refund />} />
-            <Route path="*" element={<NotFound />} />
+            <Route element={<AppLayout />}>
+                <Route path="/" element={<Refund />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
         </Routes>
     );
 }
