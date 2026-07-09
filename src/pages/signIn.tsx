1a163f1649cb9c59f4be06551ad64df57eb01ddc
Clara Nascimento <clara.nasc13@gmail.com>
2026-07-09 16:59:22 -0300

    feat: add custom Input component, signIn page, and path alias configuration

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,5 +1,21 @@
-export function SignIn(){
-    return (
-        <h1>SignIn</h1>
-    )
+import { Input } from "@/components/Input";
+
+export function SignIn() {
+  return (
+    <form className="w-full flex flex-col gap-4">
+      <Input
+        required
+        legend="email"
+        type="email"
+        placeholder="Digite seu e-mail"
+      />
+
+      <Input
+        required
+        legend="senha"
+        type="password"
+        placeholder="Digite sua senha"
+      />
+    </form>
+  );
 }
