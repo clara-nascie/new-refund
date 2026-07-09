4c44123cecfa879e51bfe3cacd506871e4e06871
Clara Nascimento <clara.nasc13@gmail.com>
2026-07-09 16:03:54 -0300

    feat: implement auth layout and routing with branded logo

---
+++ src/components/AuthLayout.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router";
+import logoIcon from "../assets/logo-icon/logoIcon.svg";
+
+//serve para deixar layout padrão para todas as telas de auth
+export function AuthLayout() {
+  return (
+    <div
+      className="w-screen h-screen bg-gray-400 flex flex-col justify-center items-center
+    text-gray-100"
+    >
+      <main className="bg-gray-500 p-8 rounded-md flex items-center flex-col md:min-w-[482px]">
+        <img src={logoIcon} alt="Logo" className="my-8" />
+
+        <Outlet />
+      </main>
+    </div>
+  );
+}
--- src/routes/auth-routes.tsx
+++ src/routes/auth-routes.tsx
@@ -1,13 +1,17 @@
 //neste arquivo ficam as rotas de autenticação
 
 import { Routes, Route } from 'react-router';
+
+import { AuthLayout } from '../components/AuthLayout';
 import { SignIn } from '../pages/signIn';
 
 
 export function AuthRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<SignIn />} />
+            <Route path="/" element={<AuthLayout />}>
+                <Route path="/" element={<SignIn />} />
+            </Route>
         </Routes>
     );
 }
