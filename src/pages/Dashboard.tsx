7db80173d91a57ce2603f47154bdb6a14b1b94e2
Clara Nascimento <clara.nasc13@gmail.com>
2026-07-10 20:23:16 -0300

    feat: create initial Dashboard page with refund search form

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,27 @@
+import { Input } from "@/components/Input";
+import { useState } from "react";
+
 export function Dashboard() {
+  const [name, setName] = useState("");
+
+  function FetchRefunds(e: React.FormEvent) {
+    e.preventDefault();
+    console.log(name);
+  }
+
   return (
     <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
-      <h1 className="text-gray-100 font-bold text-xl flex-1">
-        Suas solicitações
-      </h1>
+      <h1 className="text-gray-100 font-bold text-xl">Suas solicitações</h1>
+      <form
+        onSubmit={FetchRefunds}
+        className="flex items-center justify-between pb-6 border-b
+      border-b-gray-400 md:flex-row gap-2 mt-6"
+      >
+        <Input
+          placeholder="Pequisar pelo nome"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
     </div>
   );
 }
