8086d086a1d34e45f6843a80d1111b8cbfd6e8a0
Clara Nascimento <clara.nasc13@gmail.com>
2026-07-11 17:38:17 -0300

    feat: implement authentication routing structure, loading state, and layout component

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -6,9 +6,9 @@ export function AuthLayout() {
   return (
     <div
       className="w-screen h-screen bg-gray-400 flex flex-col justify-center items-center
-    text-gray-100"
+    text-gray-100 p-8"
     >
-      <main className="bg-gray-500 p-8 rounded-md flex items-center flex-col md:min-w-[482px]">
+      <main className="bg-gray-500 p-8 rounded-md flex items-center flex-col w-full md:min-w-[482px]">
         <img src={logoIcon} alt="Logo" className="my-8" />
 
         <Outlet />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,25 @@
 //nesse arquivo decidimos qual tipo de rota o usuário vai usar dependendo se está logado ou não
 
 import { BrowserRouter } from 'react-router';
-// import { AuthRoutes } from './AuthRoutes';
-// import { EmployeeRoutes } from './EmployeeRoutes';
+
+import { Loading } from '@/components/Loading';
+
+import { AuthRoutes } from './AuthRoutes';
+import { EmployeeRoutes } from './EmployeeRoutes';
 import { ManagerRoutes } from './ManagerRoutes';
 
-//serve para criar as rotas da aplicação, importando outras rotas
-export function Routes(){
-    // Altere para `true` para testar as rotas de funcionário logado (Refund)
-    // Altere para `false` para testar as rotas de autenticação (Login / Cadastro)
-    // const isLogged = true; 
+const isLoading = true
 
-    return (
-        <BrowserRouter>
-            <ManagerRoutes />
-        </BrowserRouter>
-    );
+//serve para criar as rotas da aplicação, importando outras rotas
+export function Routes() {
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+  return (
+    <BrowserRouter>
+      <AuthRoutes />
+    </BrowserRouter>
+  );
 }
