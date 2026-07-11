07ce282ad2025488424b9d25cc375c2e37a83b9f
Clara Nascimento <clara.nasc13@gmail.com>
2026-07-11 17:50:10 -0300

    feat: implement authentication layout and dynamic route navigation based on user roles

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -5,10 +5,10 @@ import logoIcon from "../assets/logo-icon/logo-Icon.svg";
 export function AuthLayout() {
   return (
     <div
-      className="w-screen h-screen bg-gray-400 flex flex-col justify-center items-center
-    text-gray-100 p-8"
+      className="w-full h-full bg-gray-400 flex flex-col justify-center items-center
+    text-gray-100 p-10"
     >
-      <main className="bg-gray-500 p-8 rounded-md flex items-center flex-col w-full md:min-w-[482px]">
+      <main className="bg-gray-500 p-8 rounded-md flex items-center flex-col md:min-w-[482px]">
         <img src={logoIcon} alt="Logo" className="my-8" />
 
         <Outlet />
