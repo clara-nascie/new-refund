9c8ea8155d9e45843eaa32e27a7833963b234ae7
Clara Nascimento <clara.nasc13@gmail.com>
2026-07-11 12:35:34 -0300

    feat: create reusable Button component with variant support and loading state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,26 +2,26 @@ import { classMerge } from "@/utils/ClassMerge";
 
 type Props = React.ComponentProps<"button"> & {
   isLoading?: boolean;
-  variant?: "base" | "icon";
+  variant?: "base" | "icon" | "iconSmall";
 };
 
 const variants = {
   button: {
     base: "h-12",
     icon: "h-12 w-12",
+    iconSmall:"h-10 w-10",
   },
 };
 
 export function Button({
   children,
   isLoading,
-  type = "button",
   variant = "base",
   ...rest
 }: Props) {
   return (
     <button
-      type={type}
+      type="submit"
       disabled={isLoading}
       className={classMerge([
         "flex items-center justify-center bg-green-100 rounded-lg text-white font-semibold cursor-pointer hover:bg-green-200 transition ease-linear disabled:opacity-50 disabled:cursor-progress",
