358bd932dbb21c6375a14b2289387a38226e789c
Clara Nascimento <clara.nasc13@gmail.com>
2026-07-12 16:56:53 -0300

    feat: implement sign-in page with form validation and action state handling

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,23 +1,44 @@
 import { useActionState } from "react";
+import { z } from "zod";
+
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
+
+const signInSchema = z.object({
+  email: z.string().email({ message: "Email inválido" }),
+  password: z.string().trim().min(1, { message: "Informe a senha" }),
+});
 
 export function SignIn() {
   //serve para enviar os dados para o servidor
   async function handleSignIn(prevState: any, formData: FormData) {
-    const email = formData.get("email");
-    const password = formData.get("password");
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
 
-    console.log(email, password);
+    try {
+      // Usando o try/catch exatamente como o professor fez na aula
+      const data = signInSchema.parse({
+        email: email,
+        password: password
+      });
+
+      console.log(data);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return { message: error.issues[0].message, email, password };
+      }
+      return { message: 'Erro inesperado', email, password };
+    }
 
     // O useActionState desativa o isLoading automaticamente quando essa função termina!
     // Então vamos apenas fazer ela "demorar" 2 segundos de propósito:
     await new Promise((resolve) => setTimeout(resolve, 2000));
-
+    
     return { email, password };
   }
 
   const [state, formAction, isLoading] = useActionState(handleSignIn, { email: "", password: "" });
+
 
   return (
     <form action={formAction} className="w-full flex flex-col gap-4">
@@ -38,6 +59,11 @@ export function SignIn() {
         placeholder="Digite sua senha"
         defaultValue={String(state?.password)}
       />
+
+      <p className="text-sm text-red-600 text-center my-4 font-medium">
+        {state?.message}
+      </p>
+
       <Button type="submit" isLoading={isLoading}>
         Entrar
       </Button>
