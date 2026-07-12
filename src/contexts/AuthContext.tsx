f402523b4b89c86a487fae79b82bd3bdbc00be28
Clara Nascimento <clara.nasc13@gmail.com>
2026-07-12 18:19:29 -0300

    feat: implement authentication context and define user data transfer objects

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,11 +1,20 @@
 import { createContext, ReactNode } from "react";
+import { useState } from "react";
 import { api } from "@/services/api";
 
-export const AuthContext = createContext({});
+//serve para criar um container que vai guardar a informação do usuário
+type AuthContext = {
+  session: null | UserAPIResponse
+};
+
+export const AuthContext = createContext({} as AuthContext) 
 
 export function AuthProvider({ children }: { children: ReactNode }) {
+
+  const [session, setSession] = useState<null | UserAPIResponse>(null)
+
   return (
-    <AuthContext.Provider value={{ name: "Clara" }}>
+    <AuthContext.Provider value={{ session }}>
       {children}
     </AuthContext.Provider>
   );
