b4ec412f5686d3675de66f07f496db78507820aa
Clara Nascimento <clara.nasc13@gmail.com>
2026-07-12 12:14:39 -0300

    feat: implement sign up page with zod validation and add api gitignore

--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -1,6 +1,22 @@
 import { useState, type SubmitEvent } from "react";
+import { z, ZodError } from "zod";
+
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
+
+const signupSchema = z.object({
+  name: z.string().trim().min(3, "O nome deve ter pelo menos 3 caracteres"),
+  email: z.string().trim().toLowerCase().email("Email inválido"),
+  password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
+  confirmPassword: z
+    .string()
+    .min(6, "A senha deve ter pelo menos 6 caracteres"),
+  //refine é usado para validações personalizadas
+  //ele verifica se a senha é igual a confirmação de senha
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "As senhas não coincidem",
+  path: ["confirmPassword"],
+});
 
 export function SignUp() {
   const [name, setName] = useState("");
@@ -9,17 +25,30 @@ export function SignUp() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  function onSubmit(e: SubmitEvent) {
+  //Validação de dados antes de enviar para o backend
+  function onSubmit(e: React.FormEvent) {
     e.preventDefault();
 
-    setIsLoading(true);
+    try {
+      setIsLoading(true);
 
-    setTimeout(() => {
+      const data = signupSchema.parse({
+        name,
+        email,
+        password,
+        confirmPassword,
+      });
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return alert(error.issues[0].message);
+      }
+
+      alert("Ocorreu um erro inesperado, tente novamente!");
+    } finally {
       setIsLoading(false);
-    }, 2000);
-
-    console.log(name, email, password, confirmPassword);
+    }
   }
+
   return (
     <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input
@@ -58,9 +87,13 @@ export function SignUp() {
         Criar Conta
       </Button>
 
-      <a href="/" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center
+      <a
+        href="/"
+        className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center
       hover:text-green-800 transition ease-linear"
-      >Já tem uma conta? Faça login!</a>
+      >
+        Já tem uma conta? Faça login!
+      </a>
     </form>
   );
 }
